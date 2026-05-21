5dc5def34e073308f2cf6b6621ba8e59c6ba317a
Ankit Soni <ankitsoni78870@gmail.com>
2026-05-21 12:15:58 +0530

    Added attendance session timing validation

--- server/models/Subject.js
+++ server/models/Subject.js
@@ -1,18 +1,60 @@
+// const mongoose = require('mongoose');
+
+// const subjectSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   code: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   time: {
+//     type: String 
+//   }
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('Subject', subjectSchema);
+
+
+
+
+
 const mongoose = require('mongoose');
 
 const subjectSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  code: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  time: {
-    type: String  // "10:00 AM"
-  }
+
+name: {
+type: String,
+required: true
+},
+
+code: {
+type: String,
+required: true,
+unique: true
+},
+
+time: {
+type: String
+},
+
+// NEW
+startTime: {
+type: String,
+default: ''
+},
+
+endTime: {
+type: String,
+default: ''
+}
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('Subject', subjectSchema);
+module.exports = mongoose.model(
+'Subject',
+subjectSchema
+);
+
